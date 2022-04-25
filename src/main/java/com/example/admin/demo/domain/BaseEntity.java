deede3ed37ec639415ebd68a9a159ba2853c1e4a
jihwooon <jihwooon@gmail.com>
2022-04-25 17:26:50 +0900

    setting : 환경설정

---
+++ src/main/java/com/example/admin/demo/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.admin.demo.domain;
+
+import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime createTitme;
+
+  @LastModifiedDate
+  private LocalDateTime modifiedDate;
+
+}
--- src/main/java/com/example/admin/demo/domain/FaqCategory.java
+++ src/main/java/com/example/admin/demo/domain/FaqCategory.java
@@ -1,4 +1,15 @@
 package com.example.admin.demo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class FaqCategory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
 }
