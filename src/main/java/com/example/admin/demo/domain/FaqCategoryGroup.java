f97202de98d63ee30a91c6839c20bdec6325095f
jihwooon <jihwooon@gmail.com>
2022-04-25 17:51:52 +0900

    feat : FaqCateGoryGroup domain 완성

--- src/main/java/com/example/admin/demo/domain/FaqCategoryGroup.java
+++ src/main/java/com/example/admin/demo/domain/FaqCategoryGroup.java
@@ -1,4 +1,23 @@
 package com.example.admin.demo.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FaqCategoryGroup {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private boolean expose = false;
+
 }
